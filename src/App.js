d639d9a45d3003edc80aa95b852beda5042c3f8f
mwchoen <cheonmyeongwon@gmail.com>
2023-07-05 19:14:01 +0900

    비디오카드 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { Outlet} from "react-router";
 import SearchHeader from "./components/SearchHeader";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function App() {
+
+  const queryClient = new QueryClient();
+
   return <>
     <SearchHeader/>
-    <Outlet/>
+    <QueryClientProvider client={queryClient}>
+      <Outlet/>
+    </QueryClientProvider>
   </>;
 }
 
 export default App;
+
+/**
+ * react query
+ *  1. react-query 자체 캐싱
+ * 2. fetch 과정 간소화
+ * 3. 다양한 hooks
+ * */
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { useParams } from "react-router";
+import {useParams} from "react-router";
+import {useQuery} from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
 
 function Videos(props) {
-    const { keyword } = useParams();
+    const {keyword} = useParams();
+    const {isLoading, error, data: videos} = useQuery(
+        ['videos', keyword], async () => {
+            return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+                .then((res) => res.json()
+                    .then((data) => data.items));
+        }
+    );
     return (
-        <div>Videos { keyword ?  `🔍${keyword}` : '🔥'}</div>
+        <>
+            Videos {keyword ? `🔍${keyword}` : '🔥'}
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Something is wrong </p>}
+            {videos &&
+                <ul>
+                    {
+                        videos.map(video => <VideoCard key={video.id} video={video}/>)
+                    }
+                </ul>
+            }
+        </>
     );
 }
 
